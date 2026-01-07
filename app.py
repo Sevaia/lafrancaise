0b36d6c3e8d13909b367bc713c2eeb1a57a305dd
Sevaia <l.dasilva@sevaia.eu>
2026-01-07 05:14:58 +0100

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,11 @@ import streamlit as st
 import streamlit.components.v1 as components
 
 # Page title
-st.set_page_config(page_title="My HTML App", layout="wide")
+st.set_page_config(
+    page_title="Accès PBI",  # Titre de la page
+    page_icon="📊",  # Icône affichée dans l'onglet du navigateur
+    layout="wide"  # Mise en page étendue
+)
 
 # Load your HTML file
 with open("popup.html", "r", encoding="utf-8") as f:
@@ -10,3 +14,4 @@ with open("popup.html", "r", encoding="utf-8") as f:
 
 # Display HTML
 components.html(html_content, height=800, scrolling=True)
+
